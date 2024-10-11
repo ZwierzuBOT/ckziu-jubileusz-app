442b09395adc191c2aaf3ed8a90bc5d222d19a75
Mateusz Zwierzchowski <zwierzchowski.mateo@gmail.com>
2024-10-11 18:54:49 +0200

    UserButton appearance

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,10 +4,10 @@ import { SignedIn } from "@clerk/nextjs";
 const Header = () => {
     return ( 
         <SignedIn>
-            <div className="w-full h-full flex justify-around items-center bg-[#494854] rounded-b-xl">   
+            <div className="w-full h-full flex justify-around items-center bg-[#494854] rounded-br-xl">   
                     <Link href="/send" className="text-3xl rounded-b-xl  w-[50%] h-full flex justify-center items-center">Send</Link>
                     
-                    <Link href="/regulations" className="text-3xl rounded-b-xl  w-[50%] h-full flex justify-center items-center">Regulations</Link>
+                    <a href="/#" className="text-3xl rounded-b-xl  w-[50%] h-full flex justify-center items-center">Regulations</a>
             </div>
         </SignedIn>
      );
