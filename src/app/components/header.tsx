15a9bdd3291085bdfac55822702fb91cf9ae2059
Mateusz Zwierzchowski <zwierzchowski.mateo@gmail.com>
2024-10-20 13:15:34 +0200

    header profile added

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -30,11 +30,12 @@ const Header = () => {
       <div
         className={`header ${
           !menuOpened
-            ? 'lg:w-[40%] w-[6rem] h-full md:h-full sm:h-full min-h-[4rem]'
+            ? 'lg:w-[50%] w-[6rem] h-full md:h-full sm:h-full min-h-[4rem]'
             : 'w-full h-[100vh] lg:h-[100vh]'
         } bg-gray-700 transition-all duration-500 flex justify-start lg:justify-around items-center ${
           !menuOpened ? 'rounded-br-xl' : 'rounded-none'
-        } relative`}
+        } relative
+         ${resized ? "flex-start flex-col pt-[20%]" : ""}`}
       >
         <button
           onClick={() => setMenuOpened((prev) => !prev)}
@@ -63,7 +64,7 @@ const Header = () => {
         <Link
           href="/"
           className={`
-                relative text-1.5xl md:text-2xl lg:text-3xl xl:text-4xl 
+                relative text-1xl md:text-1xl lg:text-2xl xl:text-3xl 
                 rounded-b-xl w-[50%] h-full justify-center items-center after:content-[''] 
                 after:absolute after:w-[1em] after:h-[0.15em] after:bg-gray-500 after:bottom-[0.7em] 
                 after:left-1/2 after:-translate-x-1/2 after:transition-all after:duration-500 after:ease-in-out 
@@ -77,12 +78,12 @@ const Header = () => {
           <Link
             href="/"
             className={` 
-              text-1.5xl md:text-2xl lg:text-3xl xl:text-4xl bg-gray-600 w-[100%] h-[15%] 
+              text-3xl bg-gray-600 w-[100%] h-[15%] 
               justify-center items-center transition-all duration-500 ease-in-out 
               transform ${
                 menuOpened ? 'opacity-100 h-[15%] translate-x-0' : 'opacity-0 h-0 -translate-x-full'
               } 
-              hover:bg-gray-800 flex absolute top-[12vh]`}
+              hover:bg-gray-800 flex`}
           >
             Strona Główna
           </Link>
@@ -90,11 +91,11 @@ const Header = () => {
 
         <a
           href="/#"
-          className="relative text-1.5xl md:text-2xl lg:text-3xl xl:text-4xl 
+          className="relative text-1xl md:text-1xl lg:text-2xl xl:text-3xl 
                 rounded-b-xl w-[50%] h-full justify-center items-center after:content-[''] 
                 after:absolute after:w-[1em] after:h-[0.15em] after:bg-gray-500 after:bottom-[0.7em] 
                 after:left-1/2 after:-translate-x-1/2 after:transition-all after:duration-500 after:ease-in-out 
-                after:rounded-full hover:after:w-[5em] hover:after:bg-[#fdee30] hover:after:left-1/2 
+                after:rounded-full hover:after:w-[3em] hover:after:bg-[#fdee30] hover:after:left-1/2 
                 hover:after:-translate-x-1/2 hidden lg:flex "
         >
           Regulamin
@@ -103,15 +104,45 @@ const Header = () => {
                     <a
                     href="#"
                     className={` 
-                      text-1.5xl md:text-2xl lg:text-3xl xl:text-4xl bg-gray-600 w-[100%] h-[15%] 
+                      text-3xl bg-gray-600 w-[100%] h-[15%] 
                       justify-center items-center transition-all duration-500 ease-in-out 
                       transform ${
                         menuOpened ? 'opacity-100 h-[15%] translate-x-0' : 'opacity-0 h-0 -translate-x-full'
                       } 
-                      hover:bg-gray-800 flex absolute top-[25vh]`}
+                      hover:bg-gray-800 flex`}
                   >
                     Regulamin
                   </a>
+        )}
+
+
+
+        <Link
+          href="/"
+          className={`
+                relative text-1xl md:text-1xl lg:text-2xl xl:text-3xl 
+                rounded-b-xl w-[50%] h-full justify-center items-center after:content-[''] 
+                after:absolute after:w-[1em] after:h-[0.15em] after:bg-gray-500 after:bottom-[0.7em] 
+                after:left-1/2 after:-translate-x-1/2 after:transition-all after:duration-500 after:ease-in-out 
+                after:rounded-full hover:after:w-[2em] hover:after:bg-[#fdee30] hover:after:left-1/2 
+                hover:after:-translate-x-1/2 hidden lg:flex`}
+        >
+          Profile
+        </Link>
+
+        {resized && (
+          <Link
+            href="/"
+            className={` 
+              text-3xl  bg-gray-600 w-[100%] h-[15%] 
+              justify-center items-center transition-all duration-500 ease-in-out 
+              transform ${
+                menuOpened ? 'opacity-100 h-[15%] translate-x-0' : 'opacity-0 h-0 -translate-x-full'
+              } 
+              hover:bg-gray-800 flex `}
+          >
+            Profile
+          </Link>
         )}
 
 
